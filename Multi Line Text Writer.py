ecd2db139943aca37ed2335e5618fe6bef000e16
Frances Leigh <fleighbaay@gmail.com>
2023-04-26 21:56:46 +0800

    Write the text and append new line character

--- Multi Line Text Writer.py
+++ Multi Line Text Writer.py
@@ -3,9 +3,10 @@ with open("mylife.txt", "w") as output_file:
 
     #Use loop to continuously ask the user to input info in new lines
     while True:
-    #Ask user to input any text to be written into the file
+        #Ask user to input any text to be written into the file
         new_line = input("Please enter any information about you in this line: ")
-    #Write the input text into the file and append a new line character at the end
-    #Ask user if they want to write more information
+        #Write the input text into the file and append a new line character at the end
+        output_file.write(new_line + "\n")
+        #Ask user if they want to write more information
 
 #Exit loop
