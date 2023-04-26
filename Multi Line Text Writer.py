5c1b12be163c506cbf242f555566d475172cb59e
Frances Leigh <fleighbaay@gmail.com>
2023-04-26 21:54:00 +0800

    Use loop and ask user to input text

--- Multi Line Text Writer.py
+++ Multi Line Text Writer.py
@@ -1,9 +1,11 @@
 #Write a new txt file and assign it as a variable
 with open("mylife.txt", "w") as output_file:
-    
-#Use loop to continuously ask the user to input info in new lines
-#Ask user to input any text to be written into the file
-#Write the input text into the file and append a new line character at the end
-#Ask user if they want to write more information
+
+    #Use loop to continuously ask the user to input info in new lines
+    while True:
+    #Ask user to input any text to be written into the file
+        new_line = input("Please enter any information about you in this line: ")
+    #Write the input text into the file and append a new line character at the end
+    #Ask user if they want to write more information
 
 #Exit loop
