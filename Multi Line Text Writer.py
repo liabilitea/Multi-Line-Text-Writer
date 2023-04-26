9e31515e9a145e304cf110fc36e202b1552cb706
Frances Leigh <fleighbaay@gmail.com>
2023-04-26 21:52:16 +0800

    Write new txt file and assign it as a variable

--- Multi Line Text Writer.py
+++ Multi Line Text Writer.py
@@ -1,8 +1,9 @@
 #Write a new txt file and assign it as a variable
-
+with open("mylife.txt", "w") as output_file:
+    
 #Use loop to continuously ask the user to input info in new lines
 #Ask user to input any text to be written into the file
 #Write the input text into the file and append a new line character at the end
 #Ask user if they want to write more information
 
- #Exit loop
+#Exit loop
