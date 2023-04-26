960decd5e8809897aafe9efaab1fdd4fbab33d12
Frances Leigh <fleighbaay@gmail.com>
2023-04-26 21:50:17 +0800

    Pseudocode

---
+++ Multi Line Text Writer.py
@@ -0,0 +1,8 @@
+#Write a new txt file and assign it as a variable
+
+#Use loop to continuously ask the user to input info in new lines
+#Ask user to input any text to be written into the file
+#Write the input text into the file and append a new line character at the end
+#Ask user if they want to write more information
+
+ #Exit loop
